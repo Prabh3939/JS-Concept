a12584c95fafca052a8df9fbe736c87748cf22b9
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-06-21 21:44:56 +0000

    datatype revision

---
+++ 01_basics/datatype_Summary.js
@@ -0,0 +1,31 @@
+//primitive 
+
+//7 types: String, numbers, boolean, null,undefined, symbol, bigInt-- to handle some big values or scientific value 
+
+//JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+
+
+// const id =Symbol('123')
+// const anotherId =Symbol('123')
+
+//console.log(id===anotherId);  //false as Symbol datatype work here and difrentiate the bothe same input and given the false as the output 
+
+
+//Reference (Non Primitive)
+
+// Array , Objects, functions 
+
+const hero= [shaktiman,ironman, doga]
+console.log(hero);
+  ///array
+
+let myObj ={
+    name:prabh,         //object
+    age:20,
+}
+console.log(myObj);
+
+const myFunction =function(){
+    console.log("hello world");
+}
+
