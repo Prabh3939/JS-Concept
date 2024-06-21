50091654393b4fd7537731728805ee52df8a4284
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-06-21 21:18:28 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 !=1);
+//simple arthematic or comparision 
+
+// console.log("2">1);
+// console.log("02">1);
+//tricky comparision as it can be boolean or number or string in any of the side of the comparision
+
+console.log(null>0);
+console.log(null ==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//===   ---- as it comapre the value and also find the datatype of the input value..
+
+console.log("2"===2);
+
+ 
