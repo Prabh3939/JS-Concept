f225cb599a81cabc193545ad77c6c8d3ef5a16cf
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-06-21 19:09:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using the newer version
+
+let name="prabh"
+let age = 18
+let isLoggedIn=false
+
+//number=>2 to the power 45
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value is not defined
+//symbol => unique
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); //object
