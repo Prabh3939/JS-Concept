9382e904d996388938855d393380ef4df02dade3
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-06-25 07:48:22 +0000

    stack and heap concept

--- 01_basics/datatype_Summary.js
+++ 01_basics/datatype_Summary.js
@@ -15,17 +15,34 @@
 
 // Array , Objects, functions 
 
-const hero= [shaktiman,ironman, doga]
-console.log(hero);
-  ///array
+// const hero= [shaktiman,ironman, doga]
+// console.log(hero);
+//   ///array
 
-let myObj ={
-    name:prabh,         //object
-    age:20,
+// let myObj ={
+//     name:prabh,         //object
+//     age:20,
+// }
+// console.log(myObj);
+
+// const myFunction =function(){
+//     console.log("hello world");
+// }
+//***********************************************************
+// stack(primitive )  heap (non primitive)
+let myyoutubename= "code with prabh"
+anothername ="code"
+
+console.log (myyoutubename)
+console.log(anothername);
+
+let userone={
+  email:"prs2121@abc.com",
+  upi :"user@ybl"
 }
-console.log(myObj);
 
-const myFunction =function(){
-    console.log("hello world");
-}
+let usertwo =userone
 
+usertwo.email="prs000@abc.com"
+console.log(userone.email);
+console.log(usertwo.email);
