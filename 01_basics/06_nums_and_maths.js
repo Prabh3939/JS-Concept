fc7acfbf90d57f558a7502b0b3ca2faeb05c01f4
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-09-13 20:25:23 +0000

    num and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score =400;
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+
+
+const otherNumber =123.4665
+//console.log(otherNumber)
+
+//console.log(otherNumber.toPrecision(3)); // this num is define the place holder of the digit which the user need the precised value or result
+
+const hundreds =1000000
+//console.log(hundreds)
+
+
+
+//++++++++++++++Maths++++++
+
+// console.log(Math)
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(5.6));  //gives heighest value
+// console.log(Math.floor(3.2)); // gives lowest value
+// console.log(Math.min(3,5,8,0));
+// console.log(Math.max(1,8,9,5,6));
+
+
+console.log(Math.random()); //giv value to 0-1
+
+console.log(Math.random()*10 +1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const max =10 ;
+const min = 5;
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min);
