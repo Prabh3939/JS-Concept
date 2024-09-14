f7a709a56778bcad33a4664713721dfbb35ff51f
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-09-14 20:18:05 +0000

    more about array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+const arr =[1,2,3,4]
+// console.log(arr);
+// const elem =["abc","def"]
+
+// const arr2 =new Array(1,2,3)
+// console.log(arr2[2]);
+
+//Arrays methods
+
+// arr.push(6)
+// arr.pop()
+arr.unshift(9) //used to enter the element to 1st place in the array
+arr.shift()
+console.log(arr);
+
