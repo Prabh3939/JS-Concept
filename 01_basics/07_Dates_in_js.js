62d79b4ea761deeb2adce0ffa85dbf025d863e0e
Prabhpreet singh <139834305+Prabh3939@users.noreply.github.com>
2024-09-14 08:24:31 +0000

    Dates in Js

---
+++ 01_basics/07_Dates_in_js.js
@@ -0,0 +1,29 @@
+// let myDate =new Date();
+
+// console.log(myDate.toDateString())  //gives date with day as a result
+
+// console.log(myDate.toISOString()) // gives date with time 
+
+// console.log(myDate.toLocaleDateString());  //gives date in slash form
+
+// console.log(typeof myDate);  // object
+
+// console.log(myDate.toJSON()) // gives date with time in hiffen 
+
+
+// let myCreatedDate =new Date(2023,0,23) //year month date 
+// year startes from 0 index in date in js 
+
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedddDate =new Date(2023,0,23,5,6)
+// console.log(myCreatedddDate.toLocaleString()); 
+
+let myCreatedddDate =new Date("01-14-2023")
+console.log(myCreatedddDate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
